5e2305b07bcdb33ba8b92627d8a7e1213b590a32
shivanand733 <shivanandkumbar5604@gmail.com>
2025-11-17 23:31:49 +0530

    create hibernatethe Address section

--- src/main/java/com/shivanand/hibernateOneToOne/Address.java
+++ src/main/java/com/shivanand/hibernateOneToOne/Address.java
@@ -13,5 +13,67 @@ public class Address {
 	
 	public Address(String street,String city, String state,int pincode ) {
 		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
 	}
+
+	
+	
+	public Address(int addrId, String street, String city, String state, int pincode) {
+		this.addrId = addrId;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+
+	public int getAddrId() {
+		return addrId;
+	}
+
+	public void setAddrId(int addrId) {
+		this.addrId = addrId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + ", street=" + street + ", city=" + city + ", state=" + state + ", pincode="
+				+ pincode + "]";
+	}
+	
+	
+	
 }
--- src/main/java/com/shivanand/hibernateOneToOne/Student.java
+++ src/main/java/com/shivanand/hibernateOneToOne/Student.java
@@ -2,5 +2,5 @@ package com.shivanand.hibernateOneToOne;
 
 public class Student {
 
-	private 
+	private int id;
 }
