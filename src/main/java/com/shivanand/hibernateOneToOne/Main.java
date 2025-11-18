a3858255da316d5e76032518f5ee912f0776ccd5
shivanand733 <shivanandkumbar5604@gmail.com>
2025-11-18 16:17:54 +0530

    featch the student details from the data base

--- src/main/java/com/shivanand/hibernateOneToOne/Main.java
+++ src/main/java/com/shivanand/hibernateOneToOne/Main.java
@@ -11,35 +11,18 @@ public class Main {
 public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 	
-	System.out.println("Eneter Street, city, state, pincode");
-	String street = scan.next();
-	String city = scan.next();
-	String state = scan.next();
-	int pincode = scan.nextInt();
-	
-	Address address = new Address(street, city, state, pincode);
-	
-	System.out.println("Enter name, email, phone number");
-	String name = scan.next();
-	String email = scan.next();
-	int phone = scan.nextInt();
-	
-	Student student = new Student(name, email, phone);
-	
-	student.setAddr(address);
 	
 //	Hiberante step to connect  object and data Base.....
-	
-//	Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-//	SessionFactory factory = configuration.buildSessionFactory();
-	
 	
 	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 	Session session = factory.openSession();
 	
 	Transaction transaction = session.beginTransaction();
 	
-	session.persist(student);
+//	session.persist(student);
+	System.out.println("Enter to featch details ");
+	Student s=session.get(Student.class, scan.nextInt());
+	System.out.println(s);
 	transaction.commit();
 	factory.close();
 	session.close();
