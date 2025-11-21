2b6bbdecf3d7941db215c4aa37b704f9ee5d57cd
shivanand733 <shivanandkumbar5604@gmail.com>
2025-11-21 13:19:03 +0530

    create the one to one mapping

--- src/main/java/com/shivanand/hibernateOneToOne/Main.java
+++ src/main/java/com/shivanand/hibernateOneToOne/Main.java
@@ -11,6 +11,20 @@ public class Main {
 public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 	
+	System.out.println("Enter name, email, phone number");
+	String name = scan.next();
+	String email = scan.next();
+	int phone = scan.nextInt();
+	Student student = new Student(name, email, phone);
+	
+	System.out.println("Eneter Street, city, state, pincode");
+	String street = scan.next();
+	String city = scan.next();
+	String state = scan.next();
+	int pincode = scan.nextInt();
+	Address address = new Address(street, city, state, pincode);
+
+	student.setAddr(address);
 	
 //	Hiberante step to connect  object and data Base.....
 	
@@ -19,10 +33,10 @@ public static void main(String[] args) {
 	
 	Transaction transaction = session.beginTransaction();
 	
-//	session.persist(student);
-	System.out.println("Enter to featch details ");
-	Student s=session.get(Student.class, scan.nextInt());
-	System.out.println(s);
+	session.persist(student);
+//	System.out.println("Enter to featch details ");
+//	Student s=session.get(Student.class, scan.nextInt());
+//	System.out.println(s);
 	transaction.commit();
 	factory.close();
 	session.close();
